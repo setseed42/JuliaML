bc9b39a0daf7be6891b341fa6ef782f39cefd15a
erikosmund <erik.osmund@stenaline.com>
2018-11-03 17:36:40 +0100

    Add label encoder and inverse transform methods for all transformers

--- transformers.jl
+++ transformers.jl
@@ -8,7 +8,7 @@ function chain_transformers(transformers)
     function trainer(array::Array{Float64})
         trained_transformers = []
         for transformer in transformers
-            trained_transformer = transformer(array)
+            trained_transformer, _ = transformer(array)
             array = trained_transformer(array)
             push!(trained_transformers, trained_transformer)
         end
@@ -27,7 +27,8 @@ end
 function add_bias()
     function trainer(array::Array{Float64})
         transform(x::Array{Float64})::Array{Float64} = hcat(x, ones(size(x, 1)))
-        transform
+        inverse_transform(x_transformed::Array{Float64})::Array{Float64} = x_transformed[:,1:end-1]
+        transform, inverse_transform
     end
     trainer
 end
@@ -37,11 +38,20 @@ function minmax_scaler(min::Float64, max::Float64)
     function trainer(array::Array{Float64})
         max_values = maximum(array, dims=1)
         min_values = minimum(array, dims=1)
-        result_range = max - min
-        scale_down(x::Array{Float64})::Array{Float64} = (x .- min_values) ./ (max_values .- min_values)
-        scale_up(x::Array{Float64})::Array{Float64} = x .* result_range .+ min
-        transform(x::Array{Float64})::Array{Float64} = scale_up(scale_down(x))
-        transform
+
+        function scale_down(x::Array{Float64}, min, max)::Array{Float64}
+            (x .- min) ./ (max .- min)
+        end
+        function scale_up(x::Array{Float64}, min, max)::Array{Float64}
+            x .* (max .- min) .+ min
+        end
+        function transform(x::Array{Float64})::Array{Float64}
+            scale_up(scale_down(x, min_values, max_values), min, max)
+        end
+        function inverse_transform(x::Array{Float64})::Array{Float64}
+            scale_down(scale_up(x, min_values, max_values), min, max)
+        end
+        transform, inverse_transform
     end
     trainer
 end
@@ -52,7 +62,8 @@ function standard_scaler()
         mean = Statistics.mean(array, dims=1)
         std = Statistics.std(array, dims=1)
         transform(x::Array{Float64})::Array{Float64} = (x .- mean) ./ std
-        transform
+        inverse_transform(x::Array{Float64})::Array{Float64} = x .* std .+ mean
+        transform, inverse_transform
     end
     trainer
 end
@@ -74,7 +85,31 @@ function polynomial_features(degree::Int64)
             end
             init_mat
         end
-        transform
+        #TODO
+        inverse_transform(x) = x
+        transform, inverse_transform
+    end
+    trainer
+end
+
+function label_encode()
+    function trainer(array::Array{String})
+        unique_labels = unique(array)
+
+        label_map = Dict{String,Int64}()
+        for (i, key) in enumerate(unique_labels)
+            label_map[key] = i
+        end
+
+        inverse_map = Dict{Int64,String}()
+        for (key, value) in label_map
+            inverse_map[value] = key
+        end
+        get_value(key::String)::Int64 = label_map[key]
+        get_key(value::Int64)::String = inverse_map[value]
+        transform(label::Array{String})::Array{Int64} = map(get_value, label)
+        inverse_transform(value::Array{Int64})::Array{String} = map(get_key, value)
+        transform, inverse_transform
     end
     trainer
 end
